6c3215b40791ff6e427af71c4a718fdc8ed0f731
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-06-24 17:38:12 -0500

    test: additional weapon properties

---
+++ tests/test_base_weapon.py
@@ -0,0 +1,77 @@
+import pytest
+from src.base.weapon import Weapon, WeaponType
+from src.base.faction import Faction
+
+
+def get_test_faction() -> Faction:
+    return Faction(
+        tag="test_tag",
+        accuracy_bonus=5,
+        accuracy_multiplier=1.1,
+        kinetic_damage_bonus=15,
+        kinetic_damage_multiplier=1.10,
+        energy_damage_bonus=10,
+        energy_damage_multiplier=1.15,
+        kinetic_weapon_range_bonus=100,
+        kinetic_weapon_range_multiplier=1.10,
+        energy_weapon_range_bonus=200,
+        energy_weapon_range_multiplier=1.05,
+    )
+
+
+@pytest.fixture
+def kinetic_weapon():
+    return Weapon(
+        min_crew=8,
+        max_crew=14,
+        weight=5000,
+        weapon_type=WeaponType.KINETIC,
+        base_damage=100,
+        base_range=1000,
+        base_accuracy=70,
+        fabricator=get_test_faction(),
+        user=get_test_faction(),
+    )
+
+
+@pytest.fixture
+def energy_weapon():
+    return Weapon(
+        min_crew=8,
+        max_crew=14,
+        weight=5000,
+        weapon_type=WeaponType.ENERGY,
+        base_damage=100,
+        base_range=1000,
+        base_accuracy=60,
+        fabricator=get_test_faction(),
+        user=get_test_faction(),
+    )
+
+
+def test_kinetic_weapon_damage(kinetic_weapon):
+    assert kinetic_weapon.damage == 125
+
+
+def test_energy_weapon_damage(energy_weapon):
+    assert energy_weapon.damage == 125
+
+
+def test_kinetic_weapon_range(kinetic_weapon):
+    assert kinetic_weapon.range == 1200
+
+
+def test_energy_weapon_range(energy_weapon):
+    assert energy_weapon.range == 1250
+
+
+def test_kinetic_weapon_type(kinetic_weapon):
+    assert kinetic_weapon.type == WeaponType.KINETIC
+
+
+def test_energy_weapon_type(energy_weapon):
+    assert energy_weapon.type == WeaponType.ENERGY
+
+
+def test_accuracy(kinetic_weapon):
+    assert kinetic_weapon.accuracy == 82
