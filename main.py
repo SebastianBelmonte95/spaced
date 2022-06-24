6790fa7d91256c2bf3eedf78ed19a16f37528580
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-06-24 15:16:58 -0500

    chore: cleared main fnc

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from src.buildings.shipyard import Shipyard
-from src.ships.factories import HumanShipFactory
-
-
 def main():
-    shipyard = Shipyard(crew_capacity=1500, crew=1500, repair_bays=2, building_bays=2)
-
-    small_ship = shipyard.build_ship(ship_factory=HumanShipFactory())
-    print(small_ship)
+    return
 
 
 if __name__ == "__main__":
