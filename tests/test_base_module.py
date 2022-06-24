2acee60e619582f9527bcf3fa4ef11a4b4087474
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-06-24 17:58:30 -0500

    test: test user & fabricator properties

--- tests/test_base_module.py
+++ tests/test_base_module.py
@@ -1,19 +1,32 @@
 import pytest
 from src.base.module import Module
+from tests.conftest import get_test_faction
 
 
 @pytest.fixture
 def base_module():
-    return Module(min_crew=10, max_crew=20, weight=200)
+    return Module(
+        min_crew=10,
+        max_crew=20,
+        weight=200,
+        fabricator=get_test_faction(),
+        user=get_test_faction(),
+    )
 
 
 def test_min_crew(base_module):
-    assert base_module.min_crew() == 10
+    assert base_module.min_crew == 10
 
 
 def test_max_crew(base_module):
-    assert base_module.max_crew() == 20
+    assert base_module.max_crew == 20
 
 
 def test_weight(base_module):
-    assert base_module.weight() == 200
+    assert base_module.weight == 200
+    
+def test_fabricator(base_module):
+    assert base_module.fabricator == "Faction"
+    
+def test_user(base_module):
+    assert base_module.user == "Faction"
