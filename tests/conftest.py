47c2695c2e5875d25c3c39a5497a65593bc7051d
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-06-24 21:30:48 -0500

    Added session scoped module fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
+import pytest
 from src.base.faction import Faction
+from src.base.module import Module
+
 
 def get_test_faction() -> Faction:
     return Faction(
@@ -13,4 +16,16 @@ def get_test_faction() -> Faction:
         kinetic_weapon_range_multiplier=1.10,
         energy_weapon_range_bonus=200,
         energy_weapon_range_multiplier=1.05,
+    )
+
+
+@pytest.fixture(scope="session")
+def module():
+    return Module(
+        min_crew=10,
+        max_crew=20,
+        weight=200,
+        fabricator=get_test_faction(),
+        user=get_test_faction(),
+        hp=1000,
     )
