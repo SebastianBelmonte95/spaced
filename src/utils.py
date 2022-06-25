81f273bd9543afb3a6441d71e5e0fb8168c2dcfd
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-06-24 23:31:21 -0500

    fix: set get_position to callable instead of property

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,6 @@ from math import sqrt
 
 def distance_between(entity1, entity2) -> float:
     return sqrt(
-        (entity2.get_position.x - entity1.get_position.x) ** 2
-        - (entity2.get_position.y - entity1.get_position.y) ** 2
+        (entity2.get_position().x - entity1.get_position().x) ** 2
+        + (entity2.get_position().y - entity1.get_position().y) ** 2
     )
