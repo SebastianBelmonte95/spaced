77406d540d2e2a3477d1481433f9e526dd69cd1d
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-06-24 22:03:37 -0500

    feat: added module Position

--- src/base/module.py
+++ src/base/module.py
@@ -1,5 +1,8 @@
 from abc import ABC
+from typing import Tuple
 from src.base.faction import Faction
+from src.base.position import Position
+from src.exceptions.position import NoPositionException
 
 
 class Module(ABC):
@@ -11,6 +14,7 @@ class Module(ABC):
         fabricator: Faction,
         user: Faction,
         hp: int,
+        position: Position,
         module_name: str = "Module",
     ) -> None:
         self._min_crew = min_crew
@@ -20,6 +24,7 @@ class Module(ABC):
         self._user = user
         self._hp = hp
         self._name = module_name
+        self._position = position
 
     def __str__(self) -> str:
         return self._name
@@ -47,3 +52,12 @@ class Module(ABC):
     @property
     def hp(self) -> int:
         return self._hp
+
+    def set_position(self, new_position: Position) -> None:
+        self._position = new_position
+
+    def get_position(self) -> Position:
+        return self._position
+
+    def position(self) -> Tuple[float, float]:
+        return self._position.x, self._position.y
